94f6996553ccb964e5addd049c1da6b395b5c479
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-06-04 23:00:19 +0530

    build chat Application

---
+++ src/controller/ChatViewController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class ChatViewController {
+    public Label lblName;
+    public TextField txtmsgtype;
+    public VBox vBox;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+
+    public void imageSendOnMouseclicked(MouseEvent mouseEvent) {
+    }
+}
