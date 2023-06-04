e81b2421a0380c0f6dd5b164d13ce785340d17fa
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-06-04 18:00:41 +0530

    Build up chat Application

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtloginName;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+
+    }
+}
