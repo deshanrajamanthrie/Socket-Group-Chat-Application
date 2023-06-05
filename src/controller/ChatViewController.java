8073c340e4cb6aa806518cedf19fd38a8acb933f
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-06-05 15:21:07 +0530

    build my chat Application

--- src/controller/ChatViewController.java
+++ src/controller/ChatViewController.java
@@ -7,15 +7,36 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ChatViewController {
     public Label lblName;
     public TextField txtmsgtype;
     public VBox vBox;
     public AnchorPane chatViewcontext;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
 
+    public void initialize() {
 
-    public  void initialize(){
+        String userName = LoginFormController.userName;    //lbl to set the user Name
+        lblName.setText(userName);
+        try {
+            socket = new Socket("localhost", 5001);
+            System.out.println("Local Socket is connected with  Main Server");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
