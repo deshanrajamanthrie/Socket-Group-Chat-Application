bba8757e7c42745e34a65dcc64acaf4b9f09536e
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-06-05 20:19:44 +0530

    build up my chat application

--- src/controller/ChatViewController.java
+++ src/controller/ChatViewController.java
@@ -1,19 +1,21 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
-public class ChatViewController {
+public class ChatViewController extends Thread {
     public Label lblName;
     public TextField txtmsgtype;
     public VBox vBox;
@@ -29,18 +31,63 @@ public class ChatViewController {
         lblName.setText(userName);
         try {
             socket = new Socket("localhost", 5001);
-            System.out.println("Local Socket is connected with  Main Server");
+            System.out.println("Local Socket is connected with  Main Server!");
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(socket.getOutputStream());
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String msg = reader.readLine();
+                String[] split = msg.split("");   //Split can return 0 length  string array
+                String s = split[0];    //Index 0 array
+
+                StringBuilder completemsg = new StringBuilder();    //these method can  be reprsesent capacity in maseges and initial capacity is 16
+                for (int i = 0; i < s.length(); i++) {
+                    completemsg.append(split[i] + "");
+                }
+
+                String[] split1 = msg.split("");
+                String st = "";
+                for (int i = 0; i < split1.length; i++) {
+                    st += split1[i + 1] + "";
+                }
+                Text text = new Text(st);
+                String firstChars = "";
+                if (st.length() > 3) {
+                    firstChars = st.substring(0, 3);
+                }
+                if (firstChars.equalsIgnoreCase("img")) {
+                    st = st.substring(3, st.length() - 1);
+
+                    File file = new File(st);
+                    Image image = new Image(file.toURI().toString());
+
+                    ImageView imageView = new ImageView(image);
+                    imageView.setFitWidth(150);
+                    imageView.setFitHeight(100);
+
+                    HBox hBox = new HBox(12);
+                    hBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
 
     }
 
     public void sendOnAction(ActionEvent actionEvent) {
+
     }
 
     public void imageSendOnMouseclicked(MouseEvent mouseEvent) {
